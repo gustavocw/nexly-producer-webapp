3789c66a1f3d65a9ab7cccfebd7d6903fa92494b
gustavocw <optimumsistemasbrasil@gmail.com>
2024-12-26 18:07:05 -0300

    feat: initial integrations

--- src/pages/producer/integrations/index.tsx
+++ src/pages/producer/integrations/index.tsx
@@ -1,9 +1,22 @@
-import { Box } from "@chakra-ui/react"
+import { Stack, VStack } from "@chakra-ui/react";
+import SearchBar from "components/search/search";
+import Text from "components/text/text";
+import { products } from "utils/productdummy";
 
 const Integrations = () => {
   return (
-    <Box>Integrations</Box>
-  )
-}
+    <Stack gap="32px" px={8}>
+      <VStack align="flex-start" justify="center" spaceY={5} py={5}>
+        <Text.Medium fontSize="24px">
+          Integrações ({products.length})
+        </Text.Medium>
+        <SearchBar placeholder="Pesquisar integrações" />
+      </VStack>
+      <Stack w="100%">
+        
+      </Stack>
+    </Stack>
+  );
+};
 
-export default Integrations
+export default Integrations;
